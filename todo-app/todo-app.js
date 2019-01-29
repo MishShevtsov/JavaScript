6ab90ca0c195a69c57423d2447b090765eea8f45
Mykhailo Shevtsov <shevtsov.misha19@gmail.com>
2019-01-29 20:38:30 +0200

    first commit

---
+++ todo-app/todo-app.js
@@ -0,0 +1,36 @@
+const todos = [{
+    text: 'Order cat food',
+    completed: false
+}, {
+    text: 'Clean kitchen',
+    completed: true
+}, {
+    text: 'Buy food',
+    completed: true
+}, {
+    text: 'Do work',
+    completed: false
+}, {
+    text: 'Exercise',
+    completed: true
+}]
+
+const incompleteTodos = todos.filter(function (todo) {
+    return !todo.completed
+})
+
+const summary = document.createElement('h2')
+summary.textContent = `You have ${incompleteTodos.length} todos left`
+document.querySelector('body').appendChild(summary)
+
+todos.forEach(function (todos) {
+    const p = document.createElement('p')
+    p.textContent = todos.text
+    document.querySelector('body').appendChild(p)
+})
+
+
+// Listen for new todo creation
+document.querySelector('#add-todo').addEventListener('click', function(e) {
+    console.log('Add a new todo...')
+})
